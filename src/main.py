fb6baef3b9b2e5ff6603161f7f648ecca663e103
Pontanami <pontusen@student.chalmers.se>
2023-05-19 22:42:06 +0200

    code cleanup

--- src/main.py
+++ src/main.py
@@ -6,14 +6,8 @@ import functools
 #Local imports
 import statistics
 
-
-def main():
-    print("Main")
-    time_with_decorator()
-
 def time_function(func):
     if isinstance(func, types.LambdaType):
-        # The function is passed as a lambda expression
         start_time = timeit.default_timer()
         func()
         end_time = timeit.default_timer()
@@ -27,23 +21,20 @@ def time_function(func):
 def timer(func):
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
-        tic = time.perf_counter()
+        start = time.perf_counter()
         value = func(*args, **kwargs)
-        toc = time.perf_counter()
-        time_taken = toc - tic
+        end = time.perf_counter()
+        time_taken = start - end
         print()
         print(f"Elapsed time: {time_taken:0.4f} seconds")
         return value
 
     return wrapper_timer
-
 @timer
-def time_with_decorator():
+def main():
     data = [70, 300, 250, 104, 250, 80, 135, 205, 150, 170, 284, 112, 140, 38, 123, 64, 100, 112, 96, 310, 100, 359,
             144, 147, 109, 270, 134, 108, 90, 260, 53, 145, 100, 75, 150, 207, 67, 162, 135, 140, 150, 80, 97]
     statistics.analyze_data_with_CI(data, 0.9, 10, False)
-
-
 
 if __name__ == "__main__":
     main()
