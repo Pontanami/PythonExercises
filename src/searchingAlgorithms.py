ab807fd0b5352cc9308798b54e8eb3fb3f11d3b3
Pontanami <pontusen@student.chalmers.se>
2023-05-20 20:46:01 +0200

    binary search

---
+++ src/searchingAlgorithms.py
@@ -0,0 +1,14 @@
+
+
+def binary_search(array: list, target: str):
+    left = 0
+    right = len(array) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        if array[middle] == target:
+            return middle
+        elif array[middle] < target:
+            left = middle + 1
+        else:
+            right = middle - 1
+    return -1
