1b53e39539e09c241fa627c4d05aaae9ac1c00bb
Pontanami <pontusen@student.chalmers.se>
2023-05-23 23:46:45 +0200

    pass function with arguments and not lambda

--- src/util.py
+++ src/util.py
@@ -4,8 +4,11 @@ import random
 def swap(array, i, j):
     array[i], array[j] = array[j], array[i]
 
-def shuffle_array(array: list, randomness: int):
+def shuffle_array(array: list, randomness: float):
     for i in range(len(array)):
         j = random.randint(0, len(array) - 1)
         if random.random() <= randomness:
             swap(array, i, j)
+
+def print_line():
+    print("--------------------------------------------------")
